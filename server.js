22fb0c13f79145ccdb1dfd0d750f5b02c33fb8d2
Bailey <baileylritchie@gmail.com>
2021-03-25 17:46:37 -0500

    prep for deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: [`${process.env.FRONTEND_URI_PROD || `http://localhost:3000/`}`],
   credentials:true
 }));
 
